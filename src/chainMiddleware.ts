7f996a780c4febb5ab86e2564dc9175ae112955c
Liam Martens <hi@liammartens.com>
2020-09-04 17:27:02 +0200

    Generic chainMiddleware

--- src/chainMiddleware.ts
+++ src/chainMiddleware.ts
@@ -1,12 +1,12 @@
 import type { NextApiResponse, NextApiRequest } from 'next';
 
-type NextFunc = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
-type MiddlewareFunc = (req: NextApiRequest, res: NextApiResponse, next: NextFunc) => Promise<void>;
-export const chainMiddleware = async (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  middleware: MiddlewareFunc[],
-  next: NextFunc,
+type NextFunc<RQ extends NextApiRequest = NextApiRequest, RS extends NextApiResponse = NextApiResponse> = (req: RQ, res: RS) => Promise<void>;
+type MiddlewareFunc<RQ extends NextApiRequest = NextApiRequest, RS extends NextApiResponse = NextApiResponse> = (req: RQ, res: RS, next: NextFunc) => Promise<void>;
+export const chainMiddleware = async <RQ extends NextApiRequest = NextApiRequest, RS extends NextApiResponse = NextApiResponse>(
+  req: RQ,
+  res: RS,
+  middleware: MiddlewareFunc<RQ, RS>[],
+  next: NextFunc<RQ, RS>,
 ) => {
   if (middleware.length === 1) {
     await middleware[0](req, res, next);
